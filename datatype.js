1d39b27140fcd144c48a267f919d2f88fb1ddea2
Your Name <your-email@gmail.com>
2025-12-31 20:10:52 +0530

    1

---
+++ datatype.js
@@ -0,0 +1,14 @@
+// number => 2 to power 53
+// bigint
+// string=> ""
+// boolean => true/ false
+// null => standalone value
+// undefined =>
+// symble => unique
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
+
