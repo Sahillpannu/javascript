d374d3dbec9464a7bf076ebb2f5af47c5284452f
Your Name <your-email@gmail.com>
2025-12-31 10:05:44 +0530

    1

---
+++ variable.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let  accountEmail = "sahilpannu@gmail.com"
+var  accountPassword ="357478"
+accountCity = "Hisar"
+let accountState
+
+/*accountId = 32678*/
+accountEmail = "sp@hr.com"
+accountPassword = "38483"
+accountCity = "Gurugram"
+
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+
+*/
