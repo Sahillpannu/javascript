e3c5e447aa27f8e887dbf693e05620099b82574e
Your Name <your-email@gmail.com>
2026-01-01 09:58:03 +0530

    1

---
+++ math.js
@@ -0,0 +1,41 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(200)
+//console.log(balance);
+
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(1));
+
+const otherNumber = 1213.7382
+//console.log(otherNumber.toPrecision(4));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString());
+
+
+
+
+//console.log(Math);
+//console.log(Math.abs(-5));
+//console.log(Math.round(4.4));
+//console.log(Math.ceil(4.4));
+//console.log(Math.floor(4.9));
+//console.log(Math.min(4,4,2,9,8));
+//console.log(Math.max(4,4,9,1,3));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+ 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max-min+1)+min));
+
+
+
+
+
+
+
