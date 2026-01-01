87fc1bbcca73ad305bbdb092e5db97ea55d2a3e5
Your Name <your-email@gmail.com>
2026-01-01 21:31:08 +0530

    1

---
+++ dates.js
@@ -0,0 +1,26 @@
+let myDate = new Date()
+//console.log(myDate)
+//console.log(myDate.toDateString())
+//console.log(myDate.toLocaleDateString())
+//console.log(myDate.toJSON())
+//console.log(myDate.toString())
+
+//let MyCreateDate = new Date(2026, 0 , 30, 5, 3)
+//let MyCreateDate = new Date(2026, 0 , 30)
+//let MyCreateDate = new Date("2023-01-14")
+let MyCreateDate = new Date("01-14-2026")
+//console.log(MyCreateDate.toLocaleString())
+
+let myTimeStamp = Date.now()
+
+//console.log(myTimeStamp)
+//console.log(MyCreateDate.getTime());
+
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+console.log(newDate.getDate());
+
+
