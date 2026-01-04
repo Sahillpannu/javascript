2c9188e6acefce944896502242167eebaefd1c82
Your Name <your-email@gmail.com>
2026-01-04 17:38:40 +0530

    1

---
+++ one.js
@@ -0,0 +1,46 @@
+
+const temprature = 41
+
+//if(temprature>50){
+  //  console.log("less than 50");
+//} else{
+  //  console.log("greater than 50");   
+//}
+
+// >, <, ==, <=, >=, !=,===, !==
+
+//const score = 200
+
+//if (score > 100) {
+  //  const power = "fly"
+    //console.log(`user power:${power}`);
+    
+//}
+
+const balance = 1000
+
+//if (balance < 500) {
+//    console.log("less then 500");
+//} else if (balance < 750){
+//    console.log("less then 750");
+//} else if (balance < 1000) {
+//     console.log("less then 1000");
+//} else{
+//    console.log("less then 1200");
+//}
+
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromEmail = false
+const loggedInFromGoogle = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("allow to buy course");
+}
+
+if (loggedInFromEmail || loggedInFromGoogle) {
+    console.log("user login");
+    
+}
+
