df7ec16b8741f57974e682b4052660d635f14520
Your Name <your-email@gmail.com>
2026-01-05 12:52:36 +0530

    1

---
+++ five.js
@@ -0,0 +1,37 @@
+const coding =["java", "cpp", "javascript", "ruby", "python"]
+
+//coding.forEach( function (value) {
+  //  console.log(value);
+    
+//} )
+
+//coding.forEach( (item)=> {
+  //         console.log(item);
+           
+//})
+
+//coding.forEach( (item , index, arr)=> {
+  //         console.log(item, index, arr);
+           
+//})
+
+
+const mycoding = [
+    {
+        languageName : "java script",
+        languageFile : "js"
+    },
+     {
+        languageName : "java",
+        languageFile : "java"
+    }, 
+    {
+        languageName : "python",
+        languageFile : "py"
+    },
+]
+
+mycoding.forEach( (item)=>{
+              console.log(item.languageName);
+              
+} )
