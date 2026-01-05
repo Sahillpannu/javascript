b29ab559e19f0def1d81af24285d117f8882f518
Your Name <your-email@gmail.com>
2026-01-05 12:35:36 +0530

    1

---
+++ four.js
@@ -0,0 +1,17 @@
+const myObject = {
+    js: "java script",
+    cpp: "c++",
+    rb: "ruby",
+    swift: "swiftvby apple"
+}
+
+for (const key in myObject) {
+   // console.log(` ${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["c++", "rb", "java", "javascript"]
+for (const key in programming) {
+   
+    console.log(programming[key]);
+}
+
